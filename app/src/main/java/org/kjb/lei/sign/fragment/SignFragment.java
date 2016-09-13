73a62b8d037554d936aeec8969080baab336abc7
LinFeng <china2012syl@163.com>
2016-09-14 03:55:54 +0800

    签到与显示课表界面 初步架构

---
+++ app/src/main/java/org/kjb/lei/sign/fragment/SignFragment.java
@@ -0,0 +1,20 @@
+package org.kjb.lei.sign.fragment;
+
+import org.kjb.lei.sign.R;
+import org.kjb.lei.sign.utils.base.BaseFragment;
+
+/**
+ * Created by YuShuLinFeng on 2016/9/14.
+ */
+public class SignFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.sign_main_sign;
+    }
+
+    @Override
+    protected void afterCreate() {
+
+    }
+}
